46c0c5008c33928d7d0e7166d1651b40ab033409
Altered Art <cherokeedreemer@gmail.com>
2020-12-16 08:18:09 -0700

    fixed

---
+++ site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $bosscat = "Waffles";
+    $coolcat = "Chloe";
+    $coolcatAge = 6;
+    echo "<h1>Hello this is $bosscat</h1>";
+    echo "<hr>";
+    echo "<p>$coolcat is sleeping</p>";
+    echo "<p>$coolcat is $coolcatAge years old.</p>"
+    // data types are numbers , strings
+
+
+    ?>
+</body>
+</html>
