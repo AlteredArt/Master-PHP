0ba9bbc03829b9a853764fa1eca8416e09304e38
Jared Matta <cherokeedreemer@gmail.com>
2021-01-12 07:35:02 -0700

    starting on forms

--- index.php
+++ index.php
@@ -21,9 +21,18 @@ ini_set("display_errors", 1);
         echo "<hr>";
         echo "<p>$coolcat is sleeping</p>";
         echo "<p>$coolcat is $coolcatAge years old.</p>";
+        ?>
+
+
+        test
+
+        <?php
         echo str_replace("les", "ies", $bosscat);
+        echo "</br>";
+        echo "<p>";
         echo substr($coolcat, 0, 3);
-        echo strtoupper("MEEOWWWWW");
+        echo strtoupper("meeeOooww");
+        echo "</p>";
         echo strlen($coolcat);
         echo $coolcat[2];
         $coolcat[0] = "c";
@@ -38,17 +47,27 @@ ini_set("display_errors", 1);
         echo 40;
     //decimal float
         echo 40.6;
+        echo "</br>";
     //math
         echo (4+5) * 10;
+        echo "</br>";
         echo $num + $num;
+        echo "</br>";
         echo $num *= $num;
+        echo "</br>";
     //powers
         echo pow(2, 4);
+        echo "</br>";
         echo sqrt(122);
+        echo "</br>";
     // comparison
         echo max(12, 24);
-        echo min (10 , 7)
-
+        echo "</br>";
+        echo min (10 , 7);
+        echo "</br>";
+        echo "hello round(3.7)";
     ?>
+
+
 </body>
 </html>
