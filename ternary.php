eefbbded0552a1a242c689c3a074f102185c0e19
Altered Art <cherokeedreemer@gmail.com>
2021-05-04 08:22:26 -0600

    work

--- ternary.php
+++ ternary.php
@@ -8,21 +8,21 @@
 
 
 	//  LONG HAND TERNARY
-	$is_logged_in = true;
-	if ( $is_logged_in) {
-		$message = "welcome Back!";
-	} else {
-		$message = "Hello There!";
-	}
+	// $is_logged_in = true;
+	// if ( $is_logged_in) {
+	// 	$message = "welcome Back!";
+	// } else {
+	// 	$message = "Hello There!";
+	// }
 
 	// SHORT HAND TERNARY
-	$is_logged_in = true;
-	$name = ' Alice';
-		$message = "Welcome" . ($is_logged_in ? " back!" : "!");
-		echo $message;
+	// $is_logged_in = true;
+	// $name = ' Alice';
+	// 	$message = "Welcome" . ($is_logged_in ? " back!" : "!");
+	// 	echo $message;
 	
 	//	$name = isset($name) ? $name : " Joe";
 	//	echo $name;
-	$name = $name ?: 'Joe';
+	// $name = $name ?: 'Joe';
 
 
