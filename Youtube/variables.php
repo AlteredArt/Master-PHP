9198870d921d87a1893402ca8ec2eb70aac5b708
Altered Art <cherokeedreemer@gmail.com>
2021-06-01 10:44:55 -0600

    keep going with youtube

--- Youtube/variables.php
+++ Youtube/variables.php
@@ -1,9 +1,19 @@
 <?php
-
+// Names are case sensitive in PHP, Convention is capital names.
 // Variables
-// Constants
 $firstName = "Waffles";
-$lastName = "Matta"
+$lastName = "Matta";
 echo $firstName;
+
+// Constants
+// constants cannot be remapped
+// Call the variable to see if it has been declared.
+// define('name', 'value', 'true or false for case sensitive');
+define('STATUS_PAID', 'paid');
+echo $STATUS_PAID;
+
+
+
+
 
 ?>
