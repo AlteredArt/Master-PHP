a19e61f1172831db784912ab26b462b3b19d0f22
Altered Art <cherokeedreemer@gmail.com>
2021-06-10 08:51:08 -0600

    working

--- FIX/calculator.php
+++ FIX/calculator.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator</title>
-</head>
-<body>
+
 <h1>Calculator</h1>
     <form action="calculator.php" method="get">
         <input type="number" name="num1">
@@ -14,7 +7,4 @@
         <input type="submit">
 
     </form>
-
-    Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
-</body>
-</html>
+ <?php echo $_GET["num1"] + $_GET["num2"] ?>
